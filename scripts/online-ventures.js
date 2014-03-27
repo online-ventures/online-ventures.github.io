3aff22a7608c764b234401725b80a957187c042e
Jacob Evan Shreve <jacob@shreve.ly>
2014-03-27 10:18:32 -0400

    Updates to markup

--- scripts/online-ventures.js
+++ scripts/online-ventures.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
   $(document).foundation();
+  $('.exit-off-canvas, .right-off-canvas-toggle').on('click.fndtn.offcanvas', function() {
+    $('#sidebar').click();
+  });
 });
